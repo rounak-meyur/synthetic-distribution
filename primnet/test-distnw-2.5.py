c48a3c53a6ef3b9cb02fd4b8fc80c2e6eefd9ad6
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2021-07-21 09:33:02 -0400

    Update test-distnw-2.5.py

--- primnet/test-distnw-2.5.py
+++ primnet/test-distnw-2.5.py
@@ -33,21 +33,35 @@ from pyPowerNetworklib import GetDistNet,assign_linetype
 print("Imported modules")
 
 #%% Load a network and save as shape file
-sub  =121144
-net = GetDistNet(distpath,sub)
-assign_linetype(net)
-
+def create_shapefile(sub,path):
+    net = GetDistNet(distpath,sub)
+    assign_linetype(net)
+    nodelist = net.nodes
+    d = {'label':[net.nodes[n]['label'] for n in nodelist],
+         'load':[net.nodes[n]['load'] for n in nodelist],
+         'voltage':[net.nodes[n]['voltage'] for n in nodelist],
+         'geometry':[Point(net.nodes[n]['cord']) for n in nodelist]}
+    gdf = gpd.GeoDataFrame(d, crs="EPSG:4326")
+    gdf.to_file(path+str(sub)+"-nodelist.shp")
+    
+    
+    edgelist = net.edges
+    d = {'label':[net.edges[e]['label'] for e in edgelist],
+         'line_type':[net.edges[e]['type'] for e in edgelist],
+         'r':[net.edges[e]['r'] for e in edgelist],
+         'x':[net.edges[e]['x'] for e in edgelist],
+         'length':[net.edges[e]['geo_length'] for e in edgelist],
+         'flow':[net.edges[e]['flow'] for e in edgelist],
+         'geometry':[net.edges[e]['geometry'] for e in edgelist]}
+    gdf = gpd.GeoDataFrame(d, crs="EPSG:4326")
+    gdf.to_file(path+str(sub)+"-edgelist.shp")
+    return
 
 #%%
-d = {'label':[net.nodes[n]['label'] for n in net],
-     'load':[net.nodes[n]['load'] for n in net],
-     'voltage':[net.nodes[n]['voltage'] for n in net],
-     'geometry':[Point(net.nodes[n]['cord']) for n in net]}
-gdf = gpd.GeoDataFrame(d, crs="EPSG:4326")
-gdf.to_file(shappath+sub+"-nodelist.shp")
-
-
-
+f_done = [int(f.strip('-prim-dist.gpickle')) for f in os.listdir(distpath)]
+for s in f_done:
+    create_shapefile(s,shappath)
+    print("Network created for",s)
 
 
 
